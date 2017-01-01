3c4888178f578164b5762c64c53fd743867cea9d
Chris Edwards <chrisedwards@Chriss-MacBook-Pro.local>
2017-01-01 12:10:00 -0500

    filling out the basic tech spec for the handler, adding a spec for undefined latLng

--- test/test_index.js
+++ test/test_index.js
@@ -12,25 +12,100 @@ const { expect, config } = chai;
 config.includeStack = true;
 
 describe('The Index Lambda Handler', () => {
+
     context('with a valid lat, lng in the US', () => {
-        it.skip('finds a congressional district', (done) => {
-            done(Error('test not complete'));
+
+        const event = {
+            queryStringParameters: {
+                latLng: '40.7091106,-73.9462777'
+            }
+        };
+
+        it('finds a congressional district', (done) => {
+
+            handler(event, {}, (err, { body }) => {
+                try {
+                    const { district } = JSON.parse(body);
+                    expect(district.districtCode)
+                        .to.eq('NY-07');
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            });
         });
 
-        it.skip('sends 200', (done) => {
-            done(Error('test not complete'));
+        it('sends a statusCode 200', (done) => {
+            handler(event, {}, (err, { statusCode }) => {
+                try {
+                    expect(statusCode)
+                        .to.eq(200);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            });
         });
     });
 
     context('with a valid lat, lng outside the US', () => {
-        it.skip('sends a 404', (done) => {
-            done(Error('test not complete'));
+
+        const event = {
+            queryStringParameters: {
+                latLng: '42.318131, -82.999114'
+            }
+        };
+
+        it('sends a statusCode 404', (done) => {
+            handler(event, {}, (err, { statusCode }) => {
+                try {
+                    expect(statusCode)
+                        .to.eq(404);
+                    done();
+                } catch(err) {
+                    done(err);
+                }
+            });
         });
     });
 
     context('with an invalid lat, lng', () => {
-        it.skip('sends a 400', (done) => {
-            done(Error('test not complete'));
+
+        const event = {
+            queryStringParameters: {
+                latLng: 'fooobarrr'
+            }
+        };
+
+        it('sends a statusCode 400', (done) => {
+            handler(event, {}, (err, { statusCode }) => {
+                try {
+                    expect(statusCode)
+                        .to.eq(400);
+                    done();
+                } catch(err) {
+                    done(err);
+                }
+            });
         });
     });
-})
+
+    context('with no latLng in the qs', () => {
+
+        const event = {
+            queryStringParameters: {}
+        };
+
+        it('sends a statusCode 400', (done) => {
+            handler(event, {}, (err, { statusCode }) => {
+                try {
+                    expect(statusCode)
+                        .to.eq(400);
+                    done();
+                } catch(err) {
+                    done(err);
+                }
+            });
+        });
+    });
+});
